e539b90b6fbf6356053e850731524fde7eb009bb
tomherden@gmail.com <tomherden@gmail.com>
2024-08-19 18:33:18 +0000

    Created EntryView_copy; Edited Homepage, settings and EntryView

--- client_code/EntryView_copy/__init__.py
+++ client_code/EntryView_copy/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import EntryViewTemplate
+from ._anvil_designer import EntryView_copyTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
@@ -11,18 +11,19 @@ from ..EntryEdit import EntryEdit
 from datetime import datetime
 import anvil.media
 
-class EntryView(EntryViewTemplate):
+
+class EntryView_copy(EntryView_copyTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.edit_entry_button.background = app.theme_colors['Green Button']
-    self.delete_entry_button.background = app.theme_colors['Red Button']
+    self.edit_entry_button.background = app.theme_colors["Green Button"]
+    self.delete_entry_button.background = app.theme_colors["Red Button"]
 
     # Any code you write here will run when the form opens.
 
   def edit_entry_button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    # Create a copy of the existing entry from the Data Table 
+    # Create a copy of the existing entry from the Data Table
     entry_copy = dict(self.item)
     # Open an alert displaying the 'EntryEdit' Form
     # set the `self.item` property of the EntryEdit Form to a copy of the entry to be updated
@@ -30,33 +31,31 @@ class EntryView(EntryViewTemplate):
       content=EntryEdit(item=entry_copy),
       title="Update Note",
       large=True,
-      buttons=[("Cancel", False), ("Save", True)]
+      buttons=[("Cancel", False), ("Save", True)],
     )
     # Update the entry if the user clicks save
     if save_clicked:
-      entry_copy['updated'] = datetime.now(anvil.tz.tzlocal())
-      anvil.server.call('update_entry', self.item, entry_copy)
-  
+      entry_copy["updated"] = datetime.now(anvil.tz.tzlocal())
+      anvil.server.call("update_entry", self.item, entry_copy)
+
       # Now refresh the page
       self.refresh_data_bindings()
 
   def delete_entry_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     # Get the user to confirm if they wish to delete the entry
-    # If yes, raise the 'x-delete-entry' event on the parent 
+    # If yes, raise the 'x-delete-entry' event on the parent
     # (which is the entries_panel on Homepage)
     # if confirm(f"Are you sure you want to delete {self.item['title']}?"):
-    stripped_content = self.item["content"].replace("\n"," ")
+    stripped_content = self.item["content"].replace("\n", " ")
     if len(stripped_content) >= 23:
-       stripped_content = stripped_content[:20] + "..."
+      stripped_content = stripped_content[:20] + "..."
     if confirm(f"Confirm deletion of note '{stripped_content}'"):
       self.parent.raise_event("x-delete-entry", entry=self.item)
 
   def download_button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    file_name = self.item['created'].strftime("%Y%m%d%H%M%S%z")
+    file_name = self.item["created"].strftime("%Y%m%d%H%M%S%z")
     file_contents = self.content_label.text.encode()
-    text_file = anvil.BlobMedia('text/plain', file_contents, name=f'{file_name}.md')
+    text_file = anvil.BlobMedia("text/plain", file_contents, name=f"{file_name}.md")
     anvil.media.download(text_file)
-
-
