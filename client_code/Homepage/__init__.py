8b3997dbb76cda008aac5253bf58a4c6a0ca5da1
tomherden@gmail.com <tomherden@gmail.com>
2024-09-25 18:44:12 +0000

    Edited Homepage, settings, ServerModule1 and EntryView

--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -11,17 +11,18 @@ from ..EntryEdit import EntryEdit
 from datetime import datetime
 import anvil.js.window
 
+
 class Homepage(HomepageTemplate):
   def __init__(self, **properties):
     anvil.users.login_with_form()
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Any code you write here will run when the form opens.
-    # user = anvil.users.get_user()
-    self.refresh_entries()
-      # Set an event handler on the RepeatingPanel (our 'entries_panel')
-    self.entries_panel.set_event_handler('x-delete-entry', self.delete_entry)
-    
+    # self.refresh_entries()
+    self.refresh_data_bindings()
+    # Set an event handler on the RepeatingPanel (our 'entries_panel')
+    self.entries_panel.set_event_handler("x-refresh_entries", self.refresh_entries)
+    self.entries_panel.set_event_handler("x-delete-entry", self.delete_entry)
 
   def add_entry_button_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -32,30 +33,38 @@ class Homepage(HomepageTemplate):
       content=EntryEdit(item=new_entry),
       title="Add Note",
       large=True,
-      buttons=[("Cancel", False), ("Save", True)]
+      buttons=[("Cancel", False), ("Save", True)],
     )
     # If the alert returned 'True', the save button was clicked.
     if save_clicked:
       user_time = datetime.now(anvil.tz.tzlocal())
-      anvil.server.call('add_entry', new_entry, user_time)
-      self.refresh_entries()
-    
-  def refresh_entries(self):
-     # Load existing entries from the Data Table, 
-     # and display them in the RepeatingPanel
-     self.entries_panel.items = anvil.server.call('get_entries')
+      anvil.server.call("add_entry", new_entry, user_time)
+      # self.refresh_entries()
+      self.refresh_data_bindings()
+
+  def form_refreshing_data_bindings(self, **event_args):
+    """This method is called when refreshing_data_bindings is called"""
+    self.entries_panel.items = anvil.server.call("get_entries")
+
+  def refresh_entries(self, *args, **kwargs):
+    # Load and display existing entries from the Data Table
+    self.entries_panel.items = anvil.server.call("get_entries")
+    # server_entries = anvil.server.call('get_entries')
+    # self.tag.local_entries = [dict(each) for each in server_entries]
+    # self.entries_panel.items = self.tag.local_entries
 
   def delete_entry(self, entry, **event_args):
     # Delete the entry
-    anvil.server.call('delete_entry', entry)
+    anvil.server.call("delete_entry", entry)
     # Refresh entry to remove the deleted entry from the Homepage
-    self.refresh_entries()
+    # self.refresh_entries()
+    self.refresh_data_bindings()
 
   def keepAlive_timer_tick(self, **event_args):
     """This method is called Every 1380 seconds (23 minutes)."""
-    anvil.server.call('keep_alive')
-    
+    anvil.server.call("keep_alive")
+
   def logout_button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    anvil.server.call('programmatic_logout')
+    anvil.server.call("programmatic_logout")
     anvil.js.window.location.reload(True)
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -9,19 +9,21 @@ from anvil.tables import app_tables
 import anvil.server
 from datetime import datetime
 
+
 @anvil.server.callable
 def add_entry(entry_dict, user_time):
   current_user = anvil.users.get_user()
   if current_user is not None:
-    entry_dict['content'] = encrypt(entry_dict['content'])
+    entry_dict["content"] = encrypt(entry_dict["content"])
     app_tables.entries.add_row(
       # created=datetime.now(anvil.tz.tzlocal()),
       # updated=datetime.now(anvil.tz.tzlocal()),
       created=user_time,
       updated=user_time,
-      user = current_user,
-      **entry_dict
-  )
+      user=current_user,
+      **entry_dict,
+    )
+
 
 @anvil.server.callable
 def get_entries():
@@ -29,21 +31,22 @@ def get_entries():
   # Get a list of entries from the Data Table, sorted by 'created' column, in descending order
   if current_user is not None:
     entries = app_tables.entries.search(
-      tables.order_by("created", ascending=False),
-      user = current_user
+      tables.order_by("created", ascending=False), user=current_user
     )
-    for entry in entries:
-      entry['content'] = decrypt(entry['content'])
+    for row in entries:
+      row["content"] = decrypt(row["content"])
     return entries
+
 
 @anvil.server.callable
 def update_entry(entry, entry_dict):
   # check that the entry given is really a row in the ‘entries’ table
   if app_tables.entries.has_row(entry):
-    entry_dict['content'] = encrypt(entry_dict['content'])
+    entry_dict["content"] = encrypt(entry_dict["content"])
     entry.update(**entry_dict)
   else:
     raise Exception("Note does not exist")
+
 
 @anvil.server.callable
 def delete_entry(entry):
@@ -53,19 +56,25 @@ def delete_entry(entry):
   else:
     raise Exception("Note does not exist")
 
+
 @anvil.server.callable
 def keep_alive():
   pass
+
 
 @anvil.server.callable
 def programmatic_logout():
   anvil.users.logout()
 
+
 def encrypt(plaintext):
   return anvil.secrets.encrypt_with_key("cryptkeeper", plaintext)
 
-def decrypt(ciphertext):
+
+def decrypt(possible_ciphertext):
   try:
-    return anvil.secrets.decrypt_with_key("cryptkeeper", ciphertext)
-  except anvil.secrets.SecretError:
-    return ciphertext
+    return anvil.secrets.decrypt_with_key("cryptkeeper", possible_ciphertext)
+  # except anvil.secrets.SecretError or anvil.server.InternalError:
+  except Exception as e:
+    print(e)
+    return possible_ciphertext
